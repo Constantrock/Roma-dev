f627abfb7a09f3f71f0406fae10db7f32d851536
Constantrock <4jacet@gmail.com>
2014-08-07 15:07:08 -0500

    Create Roma.py

---
+++ Roma.py
@@ -0,0 +1,477 @@
+x = 0
+y = 0
+def move():
+    while True:
+        print("to the North are %s\nto the South are %s\nto the East are %s\nto the West are %s") %(Nval+1, Sval+1, Eval, Wval+1)
+        print("Where do you want to go?")
+        c = input("> ")
+        if c == "go north":
+            y = y + 1
+            break
+        elif c == "go south":
+            y = y - 1
+            break
+        elif c == "go east":
+            x = x + 1
+            break
+        elif c == "go west":
+            x = x - 1
+            break
+        else print("I don't understand")
+def check():
+    if x = 0 and y = 0:
+        if x0y0b4 = 0:
+            print(welcome messaage)
+            x0y0b4 = 1
+        if x0y0b4 =1:
+            print("you areb ack to the city center of Anitum")
+            print("Would you like to buy anything?")
+            b = input(>" ")
+            if b == "yes":
+                print("You head to the market to look at the fine wares")
+                market()
+            if b == "no"
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
+    if x = 1 and y = 0:
+        if x1y1b4 = 0:
+        if x1y1b4 = 1:
