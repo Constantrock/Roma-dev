a458bbbdaa555002bd80ebb41dcc4020d48b199e
Constantrock <4jacet@gmail.com>
2014-11-22 14:17:27 -0600

    Create Game_Objects.py

---
+++ Game_Objects.py
@@ -0,0 +1,32 @@
+class character: #inventory items should be stored as combination of number and object
+    def __init__():
+        c = input(" >Welcome traveler what is your name\n\t:")
+        character.name = c
+        
+
+class potion:
+    def __init__(self, delta, effect):
+        self.delta = delta
+        self.effect = effect
+    def use(person, number): #store number as int
+        if person.inventorycheck(self, number) >= number: #make sure this returns int
+            person.inventoryreduce(self, number)
+            c = delta * number
+            if effect == "HP":
+                person.hp = person.hp + c
+            elif effect == "MP":
+                person.mp = person.mp + c
+            elif effect == "HP and MP":
+                person.hp = person.hp + c
+                person.mp = person.mp + c
+                
+            
+class weapon:
+
+class junk:
+    def  __init__(): #I for one haven't a clue what should go here
+    #probably just gonna have a price and a name   
+class NPC:
+
+class location:
+
